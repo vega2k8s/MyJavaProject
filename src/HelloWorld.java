7839fd8fc8e1f16f14e70ed4405430b0bb704d4f
vega2k8s <vega2k8s@gmail.com>
2025-04-23 10:52:16 +0900

    java project

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+/*
+ * javac HelloWorld.java
+ * java HelloWorld
+ * Run ¥‹√‡≈∞ : ctrl + f11
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!");
+	}
+}
