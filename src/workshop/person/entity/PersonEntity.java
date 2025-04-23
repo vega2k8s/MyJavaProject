8287335e3cc4046a6d0ecbdd205b400a905bd07f
vega2k8s <vega2k8s@gmail.com>
2025-04-23 14:43:54 +0900

    constructor add

--- src/workshop/person/entity/PersonEntity.java
+++ src/workshop/person/entity/PersonEntity.java
@@ -5,6 +5,18 @@ public class PersonEntity {
 	private char gender;
 	private String ssn;
 	private String address;
-	private String Phone;
+	private String phone;
+
+	public PersonEntity() {
+		
+	}
+
+	public PersonEntity(String name, String ssn, String address, String phone) {
+		this.name = name;
+		this.ssn = ssn;
+		this.address = address;
+		this.phone = phone;
+	}
 	
 }
+
