a7f6d63dbc7fee9ddea7a4169fdaef5955224664
vega2k8s <vega2k8s@gmail.com>
2025-04-23 11:15:20 +0900

    Account method add

--- src/workshop/account/entity/Account.java
+++ src/workshop/account/entity/Account.java
@@ -5,4 +5,38 @@ public class Account {
 	private String acctId;
 	private int balance;
 	
+	//setter
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public void setAcctId(String acctId) {
+		this.acctId = acctId;
+	}
+//	public void setBalance(int balance) {
+//		this.balance = balance;
+//	}
+	
+	//getter
+	public int getBalance() {
+		return balance;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public String getAcctId() {
+		return acctId;
+	}
+	
+	//입금 
+	public void deposit(int amount) {
+		this.balance += amount;
+	}
+	//출금
+	public void withdraw(int amount) {
+		if(amount > balance) {
+			System.out.println("잔액부족");
+		}
+		this.balance -= amount;
+	}
+	
 }
