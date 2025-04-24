7bfd22ec10c886031ac249ff8720df659a86cfd0
vega2k8s <vega2k8s@gmail.com>
2025-04-24 09:51:46 +0900

    ManageBook update

--- src/workshop/book/control/ManageBook.java
+++ src/workshop/book/control/ManageBook.java
@@ -3,6 +3,7 @@ package workshop.book.control;
 import workshop.book.entity.Magazine;
 import workshop.book.entity.Novel;
 import workshop.book.entity.Publication;
+import workshop.book.entity.ReferenceBook;
 
 public class ManageBook {
 
@@ -10,6 +11,11 @@ public class ManageBook {
 		//Publication 타입 배열을 선언 및 생성
 		Publication[] pubs = new Publication[5];
 		pubs[0] = new Magazine("마이크로소프트","2007-10-01",328,9900,"매월");
+		pubs[1] = new Magazine("경영과컴퓨터","2007-10-03",316,9000,"매월");
+		pubs[2] = new Novel("빠삐용","2007-07-01",396,9800,"베르나르베르베르","현대소설");
+		pubs[3] = new Novel("남한산성","2007-04-14",383,11000,"김훈","대하소설");
+		pubs[4] = new ReferenceBook("실용주의프로그래머","2007-01-14",496,25000,"소프트웨어공학");		
+		
 		
 		//Magazine 객체생성
 		Magazine mz = new Magazine();
