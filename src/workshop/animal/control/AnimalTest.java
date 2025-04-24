07cdb8913cef2f700bed3867f304fc71fc37d475
vega2k8s <vega2k8s@gmail.com>
2025-04-24 13:14:31 +0900

    AnimalTest add

--- src/workshop/animal/control/AnimalTest.java
+++ src/workshop/animal/control/AnimalTest.java
@@ -8,9 +8,27 @@ public class AnimalTest {
 
 	public static void main(String[] args) {
 		//Cat 객체를 생성하는 3가지 방법
-		Cat cat1 = new Cat();
-		Pet cat2 = new Cat();
-		Animal cat3 = new Cat();
+		Cat cat1 = new Cat(); //5개
+		Pet cat2 = new Cat(); //3개
+		Animal cat3 = new Cat();//2개
+		
+		//Pet로 부터 상속 받은 메서드 
+		cat1.setName("톰");
+		System.out.println(cat1.getName());
+		cat1.play();		
+		//Animal로 부터 상속 받은 메서드 
+		cat1.eat();
+		cat1.walk();
+		
+		//Pet로 부터 상속 받은 메서드 
+		cat2.setName("미요");
+		System.out.println(cat2.getName());
+		cat2.play();
+		
+		//Animal로 부터 상속 받은 메서드
+		cat3.eat();
+		cat3.walk();
+		
 		
 
 	}
