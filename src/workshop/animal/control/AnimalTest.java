b67128f330b25e4807f72c4c253e286df55c3162
vega2k8s <vega2k8s@gmail.com>
2025-04-24 13:08:47 +0900

    AnimalTest add

---
+++ src/workshop/animal/control/AnimalTest.java
@@ -0,0 +1,18 @@
+package workshop.animal.control;
+
+import workshop.animal.entity.Animal;
+import workshop.animal.entity.Cat;
+import workshop.animal.entity.Pet;
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		//Cat 객체를 생성하는 3가지 방법
+		Cat cat1 = new Cat();
+		Pet cat2 = new Cat();
+		Animal cat3 = new Cat();
+		
+
+	}
+
+}
